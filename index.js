666c20d80e23e97b79b42b67e480c0b3e8683976
suneel-eng <simhadrisnl@gmail.com>
2021-05-05 23:43:04 +0530

    styles prased for lists

--- index.js
+++ index.js
@@ -1,66 +1,14 @@
 const StreamZip = require('node-stream-zip');
-const parseString = require('xml2js').parseString;
 const formats = require('./formats');
+const { documentParser, stylesParser, numberingParser } = require('./parser/parser');
 
 module.exports = class Document {
     constructor(file, options = {
-        editable: false
+        editable: false,
+        delText: false
     }) {
         this.file = file;
         this.options = options;
-    }
-
-    prase(data) {
-        let paras = data.match(formats['wp']);
-        let structuredText = '';
-        for (let i = 0; i <= paras.length; i++) {
-            let extractedText = '';
-            let style = '';
-            let paraId = '';
-            let marginTop = 0;
-            let marginBottom = 0;
-            var runs = paras[i]?.match(formats['wr']);
-            var styles = paras[i]?.match(formats['wjc']);
-            parseString(paras[i], function (err, result) {
-                paraId = result?.['w:p']['w:pPr'][0]['w:pStyle']?.[0]?.['$']['w:val'];
-                marginTop = result?.['w:p']['w:pPr'][0]['w:spacing']?.[0]?.['$']['w:before'] / 20;
-                marginBottom = result?.['w:p']['w:pPr'][0]['w:spacing']?.[0]?.['$']['w:after'] / 20;
-            });
-
-            if (styles !== null) {
-                let match = formats['attr'].exec(styles?.[0]);
-                if (match !== null) {
-                    style = match.groups.style
-                }
-            }
-            if (runs !== null) {
-                runs?.forEach(run => {
-                    // let text = formats['wt'].exec(run);
-                    let text = run.match(formats['wt'])?.map(function (val) {
-                        return val.replace(/<\/?w:t>/g, '');
-                    })[0];
-
-                    // let bold = formats['wb'].exec(run);
-                    let bold = run.match(formats['wb'])?.map(function (val) {
-                        return formats['wb'].test(val);
-                    })[0];
-                    var underline = formats['wu'].exec(run);
-                    let delText = run.match(formats['wdelText'])?.map(function (val) {
-                        return val.replace(/<\/?w:delText>/g, '');
-                    })[0];
-                    if (text) {
-                        extractedText += `<span style='font-weight:${bold ? 'bold' : 'normal'};text-decoration:${(underline !== null) ? 'underline' : 'none'}'>${text}</span>`;
-                        bold = false;
-                    }
-                    if (delText) {
-                        extractedText += `<del style='color:red;font-weight:${bold ? 'bold' : 'normal'}'>${delText}</del>`;
-                        bold = false;
-                    }
-                })
-            }
-            structuredText += `<p class="${paraId}" style='text-align:${style};outline:none;margin-top:${marginTop}px;'>${extractedText}</p>`;
-        }
-        return `<div contenteditable=${this.options.editable}>${structuredText}</div>`;
     }
 
     extractAsHTML() {
@@ -70,13 +18,58 @@ module.exports = class Document {
                 storeEntries: true
             });
 
-            let chunks = [], content = '', components = '';
+            let chunks = [],
+                content = '',
+                components = '',
+                styleChunks = [],
+                styleContent = '',
+                styleComponents = '',
+                numberingChunks = [],
+                numberingContent = '',
+                numberingComponents = '';
 
-            zip.on('error', () => {
+            zip.on('error', (err) => {
                 reject(err);
             })
 
             zip.on('ready', () => {
+
+                // read styles.xml file
+                zip.stream('word/styles.xml', (err, stream) => {
+                    if (err) reject(reject(err));
+
+                    stream.on('data', function (chunk) {
+                        styleChunks.push(chunk);
+                    });
+
+                    stream.on('end', () => {
+                        styleContent = Buffer.concat(styleChunks)
+                        zip.close()
+                        styleComponents = styleContent.toString();
+                        let styles = stylesParser(styleComponents);
+                        return;
+                    })
+                });
+
+                //read numbering.xml file
+
+                zip.stream('word/numbering.xml', (err, stream) => {
+                    if (err) reject(reject(err));
+
+                    stream.on('data', function (chunk) {
+                        numberingChunks.push(chunk);
+                    });
+
+                    stream.on('end', () => {
+                        numberingContent = Buffer.concat(numberingChunks)
+                        zip.close()
+                        numberingComponents = numberingContent.toString();
+                        let numberings = numberingParser(numberingComponents);
+                        console.log(numberings);
+                    })
+                });
+
+                // read document.xml file
                 zip.stream('word/document.xml', (err, stream) => {
                     if (err) reject(reject(err));
 
@@ -88,7 +81,7 @@ module.exports = class Document {
                         content = Buffer.concat(chunks)
                         zip.close()
                         components = content.toString();
-                        let text = this.prase(components);
+                        let text = documentParser(components, this.options);
                         resolve(text);
                     });
                 });
---
+++ parser/parser.js
@@ -0,0 +1,79 @@
+let formats = require('../formats');
+
+/* numbering parser */
+
+let numberingParser = (data, options) => {
+    let abstractNums = data.match(formats['wabstractNum']);
+    let abstractNumObjects = [];
+    console.log(abstractNums.length);
+    for(let i = 0; i<= abstractNums.length; i++) {
+        let abstractNumId = /w:abstractNumId="(?<id>.*?)"/gm.exec(abstractNums[i])?.groups.id;
+        abstractNumObjects.push({abstractNumId});
+    }
+    console.log(abstractNumObjects);
+    return data;
+};
+
+/* styles parser */
+let stylesParser = (data, options) => {
+    let styles = data.match(formats['wstyle']);
+    let styleObjects = [];
+    for(let i = 0; i<= styles.length; i++) {
+        let type = /w:type="(?<type>.*?)"/gm.exec(styles[i])?.groups.type;
+        let styleId = /w:styleId="(?<id>.*?)"/gm.exec(styles[i])?.groups.id;
+        let numId = formats['wnumId'].exec(styles[i])?.groups.id;
+        let ilvl = formats['wilvl'].exec(styles[i])?.groups.ilvl;
+        if(type) {
+            styleObjects.push({type: type, styleId, numId, ilvl});
+        };
+    };
+    return styleObjects;
+};
+
+/* document parser */
+let documentParser = (data, options) => {
+    let paras = data.match(formats['wp']);
+    let structuredText = '';
+    for (let i = 0; i <= paras.length; i++) {
+        let extractedText = '';
+        let style = '';
+        var runs = paras[i]?.match(formats['wr']);
+        var styles = paras[i]?.match(formats['wjc']);
+
+        if (styles !== null) {
+            let match = formats['attr'].exec(styles?.[0]);
+            if (match !== null) {
+                style = match.groups.style
+            }
+        }
+        if (runs !== null) {
+            runs?.forEach(run => {
+                // let text = formats['wt'].exec(run);
+                let text = run.match(formats['wt'])?.map(function (val) {
+                    return val.replace(/<\/?w:t>/g, '');
+                })[0];
+
+                // let bold = formats['wb'].exec(run);
+                let bold = run.match(formats['wb'])?.map(function (val) {
+                    return formats['wb'].test(val);
+                })[0];
+                var underline = formats['wu'].exec(run);
+                let delText = run.match(formats['wdelText'])?.map(function (val) {
+                    return val.replace(/<\/?w:delText>/g, '');
+                })?.[0];
+                if (text) {
+                    extractedText += `<span style='font-weight:${bold ? 'bold' : 'normal'};text-decoration:${(underline !== null) ? 'underline' : 'none'}'>${text}</span>`;
+                    bold = false;
+                }
+                if (delText && options.delText) {
+                    extractedText += `<del style='color:red;font-weight:${bold ? 'bold' : 'normal'}'>${delText}</del>`;
+                    bold = false;
+                }
+            })
+        }
+        structuredText += `<p style='text-align:${style};outline:none'>${extractedText}</p>`;
+    }
+    return `<div contenteditable=${options.editable}>${structuredText}</div>`;
+}
+
+module.exports = {documentParser, stylesParser, numberingParser};
